ca25cf4a90c710d614833fd61840deda4ce37789
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-09-27 19:35:03 +0200

    final

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import {useState, useEffect} from 'react';
 function App() {
   const[loading, setLoading] = useState(true);
   const[coins,setCoins] = useState([]);
-
+  const[value,setValue] = useState(0);
   useEffect(()=>{
     fetch("https://api.coinpaprika.com/v1/tickers").then((response)=> response.json()).then((json)=>setCoins(json));
     setLoading(false);
   }, []);
-
+  const onChange = (event)=>(
+    setValue(event.target.value)
+  );
   return (
     <div>
       <h2>These are the coins!</h2>
       {loading ? "Loading!" : null}
-      <select>
+      <input value={value} onChange={onChange} style={{width:"300px"}}type="number" placeholder="Tell me how much you have"></input>
+      <hr/>
+      <ul>
         {coins.map((coin)=>(
-          <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>
+          <li key={coin.id}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD => You can buy {Math.round(value / coin.quotes.USD.price)} Coins</li>
         ))}
-      </select>
+      </ul>
+      
     </div>
   );
 }
