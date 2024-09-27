edd916718a655d56df38b1a702b2c17d86507c03
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-09-27 19:18:04 +0200

    fetch coin api and show them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {useState, useEffect} from 'react';
+
 function App() {
+  const[loading, setLoading] = useState(true);
+  const[coins,setCoins] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response)=> response.json()).then((json)=>setCoins(json));
+    setLoading(false);
+  }, []);
+
   return (
     <div>
-      Hi
+      <h2>These are the coins!</h2>
+      {loading ? "Loading!" : null}
+      <select>
+        {coins.map((coin)=>(
+          <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>
+        ))}
+      </select>
     </div>
   );
 }
